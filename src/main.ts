ed7d138ddff8b4f7523a72eff9e034a861b31c2c
PreciousIfeaka <preciousifeaka@gmail.com>
2025-07-22 07:47:32 +0100

    chore: updated packages

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from "nestjs-pino";
 import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: "*",
